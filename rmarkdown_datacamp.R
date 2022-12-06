84efea71a1629911254200b6de0066098fc85041
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-12-05 23:42:12 -0300

    Add files via upload

--- rmarkdown_datacamp.R
+++ rmarkdown_datacamp.R
@@ -6,5 +6,14 @@
 
 # Sobre o R Markdown -----------------------------------------------------------------------------------------------------------------------
 
-
+### R Markdown é uma amigável linguagem de formatação que você pode usar para revelar insights
+### dos dados e escrever seus encontros em arquivos PDF, HTML, ou aplicativos Shiny. Nesse curso,
+### você irá aprender como criar e modificar cada elemento do arquivo R Markdown, incluindo código,
+### texto, e metadados. Você irá fazer análises com dplyr, criar visualizações com ggplot2 e redigir
+### suas análises e gráficos como relatórios. Você irá ganhar experiência prática na construção de
+### relatórios como se você trabalhasse com dados reais financeiros - aprendendo como eficientemente
+### organizar relatórios usando opções de códigos em chunks, criar listas e tabelas, e incluir 
+### índices. Pelo fim do curso, você irá ter as habilidades que você necessita para adicionar as
+### fontes das suas marcas e cores usando parâmetros e Cascading Style Sheets (CSS), para fazer
+### seus relatórios se destacar.
 
